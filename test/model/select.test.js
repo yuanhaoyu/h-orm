0b0a0eced18f0174d6488a845334ec5f7892aba8
yuanhaoyu <yhy929768691@gmail.com>
2019-08-25 21:57:46 +0800

    :feat: supportLimitIntervaland write rademe about sort/limit

--- test/model/select.test.js
+++ test/model/select.test.js
@@ -42,6 +42,12 @@ const StudentModel = horm.model('student', {
   test('all limit 1', async () => {
     expect(await StudentModel().limit(1)).toEqual([{ name: 'man', sex: 1 }])
   })
+  test('all limit 1 - 2', async () => {
+    expect(await StudentModel().limit(1, 2)).toEqual([{ name: 'ww', sex: 0 }, { name: 'adad', sex: 1}])
+  })
+  test('all limit 0 - 3', async () => {
+    expect(await StudentModel().limit(0, 3)).toEqual([{ name: 'man', sex: 1 }, { name: 'ww', sex: 0 }, { name: 'adad', sex: 1}])
+  })
   test('find id = 1', async () => {
     expect(await StudentModel().find(1)).toEqual([{ name: 'man', sex: 1 }])
   })
